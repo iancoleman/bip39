9a39fb426fe66e0c757442f2bd4c05a2a71bb703
Ian Coleman <coleman.ian@gmail.com>
2015-08-16 19:17:06 +1000

    ShadowCash networks added to bitcoinjs

---
+++ src/js/bitcoinjs-extensions.js
@@ -0,0 +1,27 @@
+bitcoin.networks.shadow = {
+  magicPrefix: '\x19ShadowCash Signed Message:\n',
+  bip32: {
+    public: 0xEE80286A,
+    private: 0xEE8031E8
+  },
+  pubKeyHash: 0x3f,
+  scriptHash: 0x7d,
+  wif: 0xbf,
+  dustThreshold: 0,
+  feePerKb: 1000,
+  estimateFee: function() { return "unused in this app" },
+};
+
+bitcoin.networks.shadowtn = {
+  magicPrefix: '\x19ShadowCash Signed Message:\n',
+  bip32: {
+    public: 0x76C0FDFB,
+    private: 0x76C1077A
+  },
+  pubKeyHash: 0x7f,
+  scriptHash: 0xc4,
+  wif: 0xff,
+  dustThreshold: 0,
+  feePerKb: 1000,
+  estimateFee: function() { return "unused in this app" },
+};
