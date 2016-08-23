33ddd9a139b191fc110bb916a48d4669b76a93c2
Dan Gershony <dan.gershony@gmail.com>
2016-08-23 15:31:22 +0100

    Add dash

--- src/js/bitcoinjs-extensions.js
+++ src/js/bitcoinjs-extensions.js
@@ -34,3 +34,14 @@ bitcoin.networks.clam = {
   pubKeyHash: 0x89,
   wif: 0x85,
 };
+
+bitcoin.networks.dash = {
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4
+  },
+  pubKeyHash: 0x4e,
+  scriptHash: 0x10,
+  wif: 0xcc,
+};
+
