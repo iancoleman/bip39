908dc06fe856fb626be668eea058e76f8a02bd9b
Zwilla <info@zwilla.de>
2017-09-04 01:55:55 +0200

    changed some wrong comma and ;
    nothing else

--- src/js/bitcoinjs-extensions.js
+++ src/js/bitcoinjs-extensions.js
@@ -1,133 +1,133 @@
 bitcoinjs.bitcoin.networks.shadow = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0xEE80286A,
     private: 0xEE8031E8
   },
   pubKeyHash: 0x3f,
   scriptHash: 0x7d,
-  wif: 0xbf,
+  wif: 0xbf
 };
 
 bitcoinjs.bitcoin.networks.shadowtn = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x76C0FDFB,
     private: 0x76C1077A
   },
   pubKeyHash: 0x7f,
   scriptHash: 0xc4,
-  wif: 0xff,
+  wif: 0xff
 };
 
 bitcoinjs.bitcoin.networks.clam = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0xa8c26d64,
     private: 0xa8c17826
   },
   pubKeyHash: 0x89,
   scriptHash: 0x00, // TODO set this correctly
-  wif: 0x85,
+  wif: 0x85
 };
 
 bitcoinjs.bitcoin.networks.crown = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
   },
   pubKeyHash: 0x00,
   scriptHash: 0x05,
-  wif: 0x80,
+  wif: 0x80
 };
 
 bitcoinjs.bitcoin.networks.dash = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
   },
   pubKeyHash: 0x4c,
   scriptHash: 0x10,
-  wif: 0xcc,
+  wif: 0xcc
 };
 
 bitcoinjs.bitcoin.networks.maza = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
   },
   pubKeyHash: 0x32,
   scriptHash: 0x09,
-  wif: 0xe0,
+  wif: 0xe0
 };
 
 bitcoinjs.bitcoin.networks.dashtn = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394
   },
   pubKeyHash: 0x8c,
   scriptHash: 0x13,
-  wif: 0xef,
+  wif: 0xef
 };
 
 bitcoinjs.bitcoin.networks.game = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
   },
   pubKeyHash: 0x26,
   scriptHash: 0x05,
-  wif: 0xa6,
+  wif: 0xa6
 };
 
 bitcoinjs.bitcoin.networks.namecoin = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
   },
   pubKeyHash: 0x34,
   scriptHash: 0x00, // TODO set this correctly
-  wif: 0x80,
+  wif: 0x80
 };
 
 bitcoinjs.bitcoin.networks.peercoin = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
   },
   pubKeyHash: 0x37,
   scriptHash: 0x00, // TODO set this correctly
-  wif: 0xb7,
+  wif: 0xb7
 };
 
 bitcoinjs.bitcoin.networks.slimcoin = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0xef6adf10,
     private: 0xef69ea80
   },
   pubKeyHash: 0x3f,
   scriptHash: 0x7d,
-  wif: 0x46,
+  wif: 0x46
 };
 
 bitcoinjs.bitcoin.networks.slimcointn = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x043587CF,
     private: 0x04358394
   },
   pubKeyHash: 0x6f,
   scriptHash: 0xc4,
-  wif: 0x57,
+  wif: 0x57
 };
 
 bitcoinjs.bitcoin.networks.dogecoin = {
@@ -138,8 +138,8 @@ bitcoinjs.bitcoin.networks.dogecoin = {
   },
   pubKeyHash: 0x1e,
   scriptHash: 0x16,
-  wif: 0x9e,
-},
+  wif: 0x9e
+};
 
 bitcoinjs.bitcoin.networks.viacoin = {
   messagePrefix: '\x18Viacoin Signed Message:\n',
@@ -149,8 +149,8 @@ bitcoinjs.bitcoin.networks.viacoin = {
   },
   pubKeyHash: 0x47,
   scriptHash: 0x21,
-  wif: 0xc7,
-},
+  wif: 0xc7
+};
 
 bitcoinjs.bitcoin.networks.viacointestnet = {
   messagePrefix: '\x18Viacoin Signed Message:\n',
@@ -160,8 +160,8 @@ bitcoinjs.bitcoin.networks.viacointestnet = {
   },
   pubKeyHash: 0x7f,
   scriptHash: 0xc4,
-  wif: 0xff,
-},
+  wif: 0xff
+};
 
 bitcoinjs.bitcoin.networks.gamerscoin = {
   messagePrefix: '\x19Gamerscoin Signed Message:\n',
@@ -171,8 +171,8 @@ bitcoinjs.bitcoin.networks.gamerscoin = {
   },
   pubKeyHash: 0x26,
   scriptHash: 0x05,
-  wif: 0xA6,
-},
+  wif: 0xA6
+};
 
 bitcoinjs.bitcoin.networks.jumbucks = {
   messagePrefix: '\x19Jumbucks Signed Message:\n',
@@ -182,8 +182,8 @@ bitcoinjs.bitcoin.networks.jumbucks = {
   },
   pubKeyHash: 0x2b,
   scriptHash: 0x05,
-  wif: 0xab,
-},
+  wif: 0xab
+};
 
 bitcoinjs.bitcoin.networks.zetacoin = {
   messagePrefix: '\x18Zetacoin Signed Message:\n',
@@ -193,38 +193,38 @@ bitcoinjs.bitcoin.networks.zetacoin = {
   },
   pubKeyHash: 0x50,
   scriptHash: 0x09,
-  wif: 0xe0,
-}
+  wif: 0xe0
+};
 
 bitcoinjs.bitcoin.networks.myriadcoin = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
   },
   pubKeyHash: 0x32,
   scriptHash: 0x00, // TODO set this correctly
-  wif: 0xb2,
+  wif: 0xb2
 };
 
 bitcoinjs.bitcoin.networks.pivx = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x022d2533,
     private: 0x0221312b
   },
   pubKeyHash: 0x1e,
   scriptHash: 0x0d,
-  wif: 0xd4,
+  wif: 0xd4
 };
 
 bitcoinjs.bitcoin.networks.pivxtestnet = {
-  messagePrefix: "unused",
+  messagePrefix: 'unused',
   bip32: {
     public: 0x3a8061a0,
     private: 0x3a805837
   },
   pubKeyHash: 0x8b,
   scriptHash: 0x13,
-  wif: 0xef,
+  wif: 0xef
 };
