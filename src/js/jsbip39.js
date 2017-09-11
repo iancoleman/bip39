cd1c704be6b683cc0bdfaf51e89ea4d69aba7ac9
Zwilla <info@zwilla.de>
2017-09-12 01:42:47 +0200

    translation changes
    automatic language detection, if at the frontend it will be changed
    
    * added functions:
        * bytesToBinary
        * deriveChecksumBits
        * entropyToMnemonic
        * generateMnemonic (unused, but let it for demonstration purposes)
        * see https://www.npmjs.com/package/bip39 and https://github.com/weilu/bip39
        * changed function name init() to initJsBip39()
        * added some uncommented console.log functions for developing reasons
        * this file depends on all other PR’s I made today

--- src/js/jsbip39.js
+++ src/js/jsbip39.js
@@ -1,6 +1,7 @@
+// jsbip39.js
 /*
  * Copyright (c) 2013 Pavol Rusnak
- *
+ * Copyright (c) 2017 Miguel Padilla
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
@@ -21,52 +22,116 @@
 
 /*
  * Javascript port from python by Ian Coleman
- *
+ * Imlementation of translation detection over localstorage by Miguel Padilla
+ * for developing reasons you can uncomment all console.log functions
  * Requires code from sjcl
  * https://github.com/bitwiseshiftleft/sjcl
  */
 
-var Mnemonic = function(language) {
 
+var Mnemonic = function (language) {
     var PBKDF2_ROUNDS = 2048;
     var RADIX = 2048;
 
     var self = this;
     var wordlist = [];
 
-    var hmacSHA512 = function(key) {
+    var hmacSHA512 = function (key) {
         var hasher = new sjcl.misc.hmac(key, sjcl.hash.sha512);
-        this.encrypt = function() {
+        this.encrypt = function () {
             return hasher.encrypt.apply(hasher, arguments);
         };
     };
 
-    function init() {
+    function initJsBip39() {
+
+        language = localStorage.getItem('translation');
+
+        if (!language) {
+            language = 'english'
+        }
+
         wordlist = WORDLISTS[language];
+        // console.log('HERE  language:' + language + 'and:' + WORDLISTS[language]) ;
+
         if (wordlist.length != RADIX) {
             err = 'Wordlist should contain ' + RADIX + ' words, but it contains ' + wordlist.length + ' words.';
             throw err;
         }
     }
 
-    self.generate = function(strength) {
+    self.generate = function (strength) {
+        // console.log('generate Mnemonic with strength of:' + strength);
         strength = strength || 128;
         var r = strength % 32;
+
         if (r > 0) {
             throw 'Strength should be divisible by 32, but it is not (' + r + ').';
         }
+
         var hasStrongCrypto = 'crypto' in window && window['crypto'] !== null;
+
         if (!hasStrongCrypto) {
             throw 'Mnemonic should be generated with strong randomness, but crypto.getRandomValues is unavailable';
         }
+
         var buffer = new Uint8Array(strength / 8);
         var data = crypto.getRandomValues(buffer);
+        // console.log('generate Mnemonic data:' + data);
         return self.toMnemonic(data);
+    };
+
+    function bytesToBinary(bytes) {
+        return bytes.map(function (x) {
+            return lpad(x.toString(2), '0', 8)
+        }).join('')
     }
 
-    self.toMnemonic = function(byteArray) {
+    function deriveChecksumBits(entropyBuffer) {
+        var ENT = entropyBuffer.length * 8;
+        var CS = ENT / 32;
+        var hash = createHash('sha256').update(entropyBuffer).digest();
+
+        return bytesToBinary([].slice.call(hash)).slice(0, CS)
+    }
+
+    function entropyToMnemonic(entropyHex, wordlist) {
+        wordlist = wordlist || DEFAULT_WORDLIST;
+
+        // 128 <= ENT <= 256
+        if (entropyHex.length < 32) throw new TypeError(INVALID_ENTROPY);
+        if (entropyHex.length > 64) throw new TypeError(INVALID_ENTROPY);
+
+        // multiple of 4
+        if (entropyHex.length % 8 !== 0) throw new TypeError(INVALID_ENTROPY);
+
+        var entropy = Buffer.from(entropyHex, 'hex');
+        var entropyBits = bytesToBinary([].slice.call(entropy));
+        var checksumBits = deriveChecksumBits(entropy);
+
+        var bits = entropyBits + checksumBits;
+        var chunks = bits.match(/(.{1,11})/g);
+        var words = chunks.map(function (binary) {
+            var index = binaryToByte(binary);
+            return wordlist[index]
+        });
+
+        return wordlist === JAPANESE_WORDLIST ? words.join('\u3000') : words.join(' ')
+    }
+
+    function generateMnemonic(strength, rng, wordlist) {
+        strength = strength || 128;
+        if (strength % 32 !== 0) throw new TypeError(INVALID_ENTROPY);
+        rng = rng || randomBytes;
+
+        var hex = rng(strength / 8).toString('hex');
+        return entropyToMnemonic(hex, wordlist)
+    }
+
+    self.toMnemonic = function (byteArray) {
+
         if (byteArray.length % 4 > 0) {
-            throw 'Data length in bits should be divisible by 32, but it is not (' + byteArray.length + ' bytes = ' + byteArray.length*8 + ' bits).'
+            throw 'Data length in bits should be divisible by 32, but it is not (' + byteArray.length + ' bytes = ' + byteArray.length * 8 + ' bits).'
         }
 
         //h = hashlib.sha256(data).hexdigest()
@@ -74,13 +139,6 @@ var Mnemonic = function(language) {
         var hash = sjcl.hash.sha256.hash(data);
         var h = sjcl.codec.hex.fromBits(hash);
 
-        // b is a binary string, eg '00111010101100...'
-        //b = bin(int(binascii.hexlify(data), 16))[2:].zfill(len(data) * 8) + \
-        //    bin(int(h, 16))[2:].zfill(256)[:len(data) * 8 / 32]
-        //
-        // a = bin(int(binascii.hexlify(data), 16))[2:].zfill(len(data) * 8)
-        // c = bin(int(h, 16))[2:].zfill(256)
-        // d = c[:len(data) * 8 / 32]
         var a = byteArrayToBinaryString(byteArray);
         var c = zfill(hexStringToBinaryString(h), 256);
         var d = c.substring(0, byteArray.length * 8 / 32);
@@ -89,26 +147,39 @@ var Mnemonic = function(language) {
 
         var result = [];
         var blen = b.length / 11;
-        for (var i=0; i<blen; i++) {
+        // var wordlist = WORDLISTS[language];
+
+        for (var i = 0; i < blen; i++) {
             var idx = parseInt(b.substring(i * 11, (i + 1) * 11), 2);
             result.push(wordlist[idx]);
+            // console.log('result.push:' + idx);
         }
+        // console.log('to Mnemonic result:' + result);
         return self.joinWords(result);
-    }
+    };
 
-    self.check = function(mnemonic) {
+    self.check = function (mnemonic) {
         var mnemonic = self.splitWords(mnemonic);
         if (mnemonic.length == 0 || mnemonic.length % 3 > 0) {
+            // console.log('here mnemonic:' + mnemonic);
             return false
         }
+
         // idx = map(lambda x: bin(self.wordlist.index(x))[2:].zfill(11), mnemonic)
         var idx = [];
-        for (var i=0; i<mnemonic.length; i++) {
+        // console.log('mnemonic.length:' + mnemonic.length);
+
+
+        for (var i = 0; i < mnemonic.length; i++) {
             var word = mnemonic[i];
             var wordIndex = wordlist.indexOf(word);
+            // console.log('wordIndex:' + wordIndex);
+
             if (wordIndex == -1) {
+                // console.log('here 2:' + wordIndex + ' Word:' + word);
                 return false;
             }
+
             var binaryIndex = zfill(wordIndex.toString(2), 11);
             idx.push(binaryIndex);
         }
@@ -124,73 +195,76 @@ var Mnemonic = function(language) {
         var ndHash = sjcl.hash.sha256.hash(nd);
         var ndHex = sjcl.codec.hex.fromBits(ndHash);
         var ndBstr = zfill(hexStringToBinaryString(ndHex), 256);
-        var nh = ndBstr.substring(0,l/33);
+        var nh = ndBstr.substring(0, l / 33);
+        // console.log('here 3:' + nh + '\nand:' + h);
         return h == nh;
-    }
+    };
 
-    self.toSeed = function(mnemonic, passphrase) {
+    self.toSeed = function (mnemonic, passphrase) {
         passphrase = passphrase || '';
         mnemonic = self.joinWords(self.splitWords(mnemonic)); // removes duplicate blanks
         var mnemonicNormalized = self.normalizeString(mnemonic);
-        passphrase = self.normalizeString(passphrase)
-        passphrase = "mnemonic" + passphrase;
+        passphrase = self.normalizeString(passphrase);
+        passphrase = 'mnemonic' + passphrase;
         var mnemonicBits = sjcl.codec.utf8String.toBits(mnemonicNormalized);
         var passphraseBits = sjcl.codec.utf8String.toBits(passphrase);
         var result = sjcl.misc.pbkdf2(mnemonicBits, passphraseBits, PBKDF2_ROUNDS, 512, hmacSHA512);
         var hashHex = sjcl.codec.hex.fromBits(result);
         return hashHex;
-    }
+    };
 
-    self.splitWords = function(mnemonic) {
-        return mnemonic.split(/\s/g).filter(function(x) { return x.length; });
-    }
+    self.splitWords = function (mnemonic) {
+        return mnemonic.split(/\s/g).filter(function (x) {
+            return x.length;
+        });
+    };
 
-    self.joinWords = function(words) {
+    self.joinWords = function (words) {
         // Set space correctly depending on the language
         // see https://github.com/bitcoin/bips/blob/master/bip-0039/bip-0039-wordlists.md#japanese
-        var space = " ";
-        if (language == "japanese") {
-            space = "\u3000"; // ideographic space
+        var space = ' ';
+        if (language == 'japanese') {
+            space = '\u3000'; // ideographic space
         }
         return words.join(space);
-    }
+    };
 
-    self.normalizeString = function(str) {
-        if (typeof str.normalize == "function") {
-            return str.normalize("NFKD");
+    self.normalizeString = function (str) {
+        if (typeof str.normalize == 'function') {
+            return str.normalize('NFKD');
         }
         else {
             // TODO decide how to handle this in the future.
             // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
             return str;
         }
-    }
+    };
 
     function byteArrayToWordArray(data) {
         var a = [];
-        for (var i=0; i<data.length/4; i++) {
+        for (var i = 0; i < data.length / 4; i++) {
             v = 0;
-            v += data[i*4 + 0] << 8 * 3;
-            v += data[i*4 + 1] << 8 * 2;
-            v += data[i*4 + 2] << 8 * 1;
-            v += data[i*4 + 3] << 8 * 0;
+            v += data[i * 4 + 0] << 8 * 3;
+            v += data[i * 4 + 1] << 8 * 2;
+            v += data[i * 4 + 2] << 8 * 1;
+            v += data[i * 4 + 3] << 8 * 0;
             a.push(v);
         }
         return a;
     }
 
     function byteArrayToBinaryString(data) {
-        var bin = "";
-        for (var i=0; i<data.length; i++) {
+        var bin = '';
+        for (var i = 0; i < data.length; i++) {
             bin += zfill(data[i].toString(2), 8);
         }
         return bin;
     }
 
     function hexStringToBinaryString(hexString) {
-        binaryString = "";
-        for (var i=0; i<hexString.length; i++) {
-            binaryString += zfill(parseInt(hexString[i], 16).toString(2),4);
+        binaryString = '';
+        for (var i = 0; i < hexString.length; i++) {
+            binaryString += zfill(parseInt(hexString[i], 16).toString(2), 4);
         }
         return binaryString;
     }
@@ -198,8 +272,8 @@ var Mnemonic = function(language) {
     function binaryStringToWordArray(binary) {
         var aLen = binary.length / 32;
         var a = [];
-        for (var i=0; i<aLen; i++) {
-            var valueStr = binary.substring(0,32);
+        for (var i = 0; i < aLen; i++) {
+            var valueStr = binary.substring(0, 32);
             var value = parseInt(valueStr, 2);
             a.push(value);
             binary = binary.slice(32);
@@ -219,6 +293,6 @@ var Mnemonic = function(language) {
         return source;
     }
 
-    init();
+    initJsBip39();
 
-}
+};
