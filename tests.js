0cca6e893af22e7b6d4e3621a8a0bf3c68c00e41
Ian Coleman <coleman.ian@gmail.com>
2017-10-10 12:04:27 +1100

    Add test for nubits network

--- tests.js
+++ tests.js
@@ -1052,6 +1052,36 @@ page.open(url, function(status) {
 });
 },
 
+// Network can be set to nubits
+function() {
+page.open(url, function(status) {
+    // set the phrase and coin
+    var expected = "BLxkabXuZSJSdesLD7KxZdqovd4YwyBTU6";
+    page.evaluate(function() {
+        $(".phrase").val("abandon abandon ability");
+        $(".phrase").trigger("input");
+        $(".network option[selected]").removeAttr("selected");
+        $(".network option").filter(function() {
+            return $(this).html() == "USNBT - NuBits";
+        }).prop("selected", true);
+        $(".network").trigger("change");
+    });
+    // check the address is generated correctly
+    waitForGenerate(function() {
+        var actual = page.evaluate(function() {
+            return $(".address:first").text();
+        });
+        if (actual != expected) {
+            console.log("NuBits address is incorrect");
+            console.log("Expected: " + expected);
+            console.log("Actual: " + actual);
+            fail();
+        }
+        next();
+    });
+});
+},
+
 // BIP39 seed is set from phrase
 function() {
 page.open(url, function(status) {
