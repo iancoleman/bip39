9369eaa6d56500061c952927691e31cd503ae11c
Ian Coleman <ian@iancoleman.io>
2018-05-30 11:32:43 +1000

    Add litecoin p2wpkh parameters
    From electrum-ltc
    https://github.com/pooler/electrum-ltc/blob/48996679e198dab0839a2613289af8ba250b4f34/lib/constants.py

--- src/js/segwit-parameters.js
+++ src/js/segwit-parameters.js
@@ -56,6 +56,19 @@ bitcoinjs.bitcoin.networks.testnet.p2wpkhInP2sh = {
     wif: 0xef
 };
 
+bitcoinjs.bitcoin.networks.litecoin.p2wpkh = {
+    baseNetwork: "litecoin",
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bech32: 'ltc',
+    bip32: {
+        public: 0x04b24746,
+        private: 0x04b2430c
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0
+};
+
 bitcoinjs.bitcoin.networks.litecoin.p2wpkhInP2sh = {
     baseNetwork: "litecoin",
     messagePrefix: '\x19Litecoin Signed Message:\n',
