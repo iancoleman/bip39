cd8893f6c980644ff382d10dee7181d1a943ebad
Matthew Cornelisse <squarerootofnegativeone@gmail.com>
2018-10-02 23:44:17 -0500

    added DigiByte segwit parameters

--- src/js/segwit-parameters.js
+++ src/js/segwit-parameters.js
@@ -159,4 +159,32 @@ bitcoinjs.bitcoin.networks.bgold.p2wpkhInP2sh = {
     wif: 0x80,
 };
 
+bitcoinjs.bitcoin.networks.digibyte.p2wpkh = {
+    baseNetwork: "digibyte",
+	messagePrefix: '\x18DigiByte Signed Message:\n',
+    bech32: 'dgb',
+    bip32: {
+		public: 0x0488B21E,
+		private: 0x0488ADE4
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x3f,
+    wif: 0x80
+};
+
+bitcoinjs.bitcoin.networks.digibyte.p2wpkhInP2sh = {
+    baseNetwork: "digibyte",	
+	messagePrefix: '\x18DigiByte Signed Message:\n',
+    bech32: 'dgb',
+    bip32: {
+		public: 0x0488B21E,
+		private: 0x0488ADE4
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x3f,
+    wif: 0x80
+};
+
+    
+    
 })();
