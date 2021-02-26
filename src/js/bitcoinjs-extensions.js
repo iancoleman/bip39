c56b4b319d212e3d4af84e51abe55677e3a34ea4
Babuxor <ilbombarolofolle@gmail.com>
2021-02-26 07:20:04 +0100

    Added Monero and Decred

--- src/js/bitcoinjs-extensions.js
+++ src/js/bitcoinjs-extensions.js
@@ -1865,3 +1865,25 @@ libs.bitcoin.networks.argoneum = {
   scriptHash: 0x61,
   wif: 0xbf
 };
+
+libs.bitcoin.networks.decred = {
+  messagePrefix: 'unused',
+  bip32: {
+    public: 0x02fda926,
+    private: 0x02fda4e8
+  },
+  pubKeyHash: 0x073f,
+  scriptHash: 0x071a,
+  wif: 0x80
+};
+
+libs.bitcoin.networks.monero = {
+    messagePrefix: 'x18XMR Signed Message:\n',
+    bip32: {
+      public: 0x0488B21E,
+      private: 0x0488ADE4,
+    },
+    pubKeyHash: 0x7F,
+    scriptHash: 0xC4,
+    wif: 0x3F,
+};
